9ffe87212f292928a12ba17169c909d4ab9d3950
hyeseon <chunghye1998@gmail.com>
2024-02-19 20:04:07 +0900

    study: day1

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,4 +1,16 @@
 package com.group.libraryapp.controller.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.group.libraryapp.controller.dto.calculator.request.CalculatorAddRequest;
+
+@RestController
 public class CalculatorController {
+
+	@GetMapping("/add") // GET /add
+	public int addTwoNumbers(CalculatorAddRequest request){
+		return request.getNumber1() + request.getNumber2();
+	}
+
 }
