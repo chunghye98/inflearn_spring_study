5a9a05129df53e509ebdf43fa37abd64199703d3
hyeseon <chunghye1998@gmail.com>
2024-02-21 22:32:01 +0900

    study: day3

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,21 +1,27 @@
 package com.group.libraryapp.controller.user;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 
 @RestController
 public class UserController {
 
-	private final List<User> users = new ArrayList<>();
+	private final JdbcTemplate jdbcTemplate;
+
+	public UserController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	/**
 	 * 유저 생성 API
@@ -23,7 +29,8 @@ public class UserController {
 	 */
 	@PostMapping("/user") // POST /user
 	public void saveUser(@RequestBody UserCreateRequest request) {
-		users.add(new User(request.getName(), request.getAge()));
+		String sql = "INSERT INTO user (name, age) VALUES (?,?)";
+		jdbcTemplate.update(sql, request.getName(), request.getAge());
 	}
 
 	/**
@@ -31,10 +38,12 @@ public class UserController {
 	 */
 	@GetMapping("/user")
 	public List<UserResponse> getUsers() {
-		List<UserResponse> responses = new ArrayList<>();
-		for (int i = 0; i < users.size(); i++) {
-			responses.add(new UserResponse(i + 1, users.get(i)));
-		}
-		return responses;
+		String sql = "SELECT * FROM user";
+		return jdbcTemplate.query(sql, (rs, rowNum) -> {
+			long id = rs.getLong("id");
+			String name = rs.getNString("name");
+			int age = rs.getInt("age");
+			return new UserResponse(id, name, age);
+		});
 	}
 }
